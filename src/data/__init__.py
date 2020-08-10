b7865f68a64fe628c1d27364b9aeac82210be054
joaopfonseca <jpmrfonseca@gmail.com>
2020-08-10 16:34:54 +0100

    Added utility function to load all results from ES query

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,6 @@
 from ._pull_data import pull_tweets, count_tweets
 from ._transform import transform
 from ._load_es import load_es
+from ._utils import query_es
 
-__all__ = ['pull_tweets', 'count_tweets', 'transform', 'load_es']
+__all__ = ['pull_tweets', 'count_tweets', 'transform', 'load_es', 'query_es']
